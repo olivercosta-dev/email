a6c9fc70b915e55898557e5cb54af811912d4b19
Oliver <costaolika@gmail.com>
2024-03-22 19:19:34 +0100

    status now works

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -59,8 +59,8 @@ pub fn is_valid_name(s: &str) -> bool {
 pub async fn insert_subscriber(new_subscriber: &NewSubscriber, pool: &PgPool) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-            INSERT INTO subscriptions (id, email, name, subscribed_at)
-            VALUES ($1, $2, $3, $4)
+            INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+            VALUES ($1, $2, $3, $4, 'confirmed')
         "#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
